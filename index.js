81a2db18ce3b4409c8144049da3edf871c04f0e3
cupof2 <arjayandal05@yahoo.com>
2021-09-21 15:28:27 -0700

    Testing

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hi there');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const start = async() => {
+    const res = await axios.get('https://www.reddit.com/r/Showerthoughts/');
+
+    const $ = cheerio.load(res.data);
+
+    $(`h3`).each((i, el) => {
+        console.log(`\n` + $(el).first().text());
+    });
+}
+
+start();
+
+// noob
+// noob
+// noob
+// noob
+// noob
+// noob
+// noob
+// noob
+// noob
+// noob
+// noob
+// noob
+// noob
+// noob
+// noob
